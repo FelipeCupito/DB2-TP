a3d98189b7ff41e7812bc5a22c8e45e0fb4c45cb
malevasquez <male.vasquez@yahoo.com.ar>
2023-07-22 10:19:55 -0300

    Changes by SolChiSol

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,11 +4,7 @@ from backend.app.routers import banks, transactions, users
 
 app = FastAPI()
 
-# crea la base de datos
-# models.Base.metadata.create_all(bind=engine)
-
-
-app.include_router(banks.router, prefix='/banks', tags=["banks"])
+# app.include_router(banks.router, prefix='/banks', tags=["banks"])
 app.include_router(transactions.router, prefix='/transactions', tags=["transactions"])
 app.include_router(users.router, prefix='/users', tags=["users"])
 
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,42 +1,19 @@
-from datetime import datetime
-from sqlalchemy import Column, String, Float, DateTime, TEXT
+from sqlalchemy import Column, String, Float, Date
 from sqlalchemy import ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
 
 from database import Base, engine
 
-
-class Bank(Base):
-    __tablename__ = "banks"
-    _id = Column(UUID(as_uuid=True), primary_key=True, unique=True)
-    name = Column(String)
-    port = Column(String)
-
-    __table_args__ = {'extend_existing': True}
-
-
 class User(Base):
     __tablename__ = "users"
-    _id = Column(String, primary_key=True, unique=True)
-    alias_type = Column(String)
-    password = Column("Password", TEXT, nullable=False)
-    name = Column(String)
-    cuit = Column(String)
-    cbu = Column(String)
-    bank_id = Column(ForeignKey("banks._id"))
+    name = Column(String, nullable=False)
+    birthday = Column(Date)
+    genre = Column(String)
+    cuit = Column(String, unique=True, nullable=False)
+    cbu = Column(String, primary_key=True, unique=True, nullable=False)
+    phone: Column(String, unique=True)
+    email: Column(String, unique=True)
+    balance: Column(Float, nullable=False)
 
     __table_args__ = {'extend_existing': True}
-
-
-class Transaction(Base):
-    __tablename__ = "transactions"
-    _id = Column(UUID(as_uuid=True), primary_key=True)
-    date = Column(DateTime, default=datetime.utcnow)
-    from_alias_id = Column(ForeignKey("users._id"))
-    to_alias_id = Column(ForeignKey("users._id"))
-    amount = Column(Float)
-
-    __table_args__ = {'extend_existing': True}
-
 
 Base.metadata.create_all(bind=engine)
