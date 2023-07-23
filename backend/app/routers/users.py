c17cf34e4ba5b84043b9afb1169a942b64abcdb4
malevasquez <male.vasquez@yahoo.com.ar>
2023-07-23 12:22:10 -0300

    Changes by SolChiSol

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -11,15 +11,27 @@ router = APIRouter()
 
 @router.get("/{cbu}", response_model=UserBase)
 def get_user_info(cbu: str, db: Session = Depends(get_db)):
-    return users_dao.get_info(cbu, db)
-    # try:
-    #     user = users_dao.get_info(cbu, db)
-    # except ValueError:
-    #     raise Exception("User information incorrectly formatted.")
-    # return user
-
+    try:
+        user = users_dao.get_info(cbu, db)
+        if user:
+            return user
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
+    except HTTPException:
+        raise
+    except ResponseValidationError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Account information wrongly formatted")
 
 
 @router.get("/{cbu}/balance", response_model=float)
 def get_user_balance(cbu: str, db: Session = Depends(get_db)):
-    return users_dao.get_balance(cbu, db)
+    try:
+        balance = users_dao.get_balance(cbu, db)
+        if balance:
+            return balance
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
+    except HTTPException:
+        raise
+    except ResponseValidationError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Account information wrongly formatted")
